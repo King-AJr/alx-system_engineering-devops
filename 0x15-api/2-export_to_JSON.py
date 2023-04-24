472681a99bc9a19bc15e5d3e05a94b5f63e675ee
King-AJr <talk2king.aj@gmail.com>
2023-04-24 22:15:08 +0000

    set format for which json values should take

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -15,10 +15,15 @@ if __name__ == '__main__':
     user_info = requests.get(user_info_url, params=params)
     employee = user_info.json()[0]
     emp_name = employee['name']
+    username = employee['username']
 
     todo_info_url = 'https://jsonplaceholder.typicode.com/todos'
     todo_info = requests.get(todo_info_url, params=todo_params)
     todos = todo_info.json()
     filename = "{}.json".format(user_id)
     with open(filename, 'w') as file:
-        json.dump(todos, file)
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, file)
