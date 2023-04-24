5bc7d45622e3ac4f18426664cbfe60054ff10a47
King-AJr <talk2king.aj@gmail.com>
2023-04-24 21:08:07 +0000

    export result in csv format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,6 +5,7 @@
 
 import requests
 import sys
+import csv
 
 if __name__ == '__main__':
     user_id = sys.argv[1]
@@ -15,10 +16,12 @@ if __name__ == '__main__':
     user_info = requests.get(user_info_url, params=params)
     employee = user_info.json()[0]
     employee_name = employee['name']
-
     todo_info_url = 'https://jsonplaceholder.typicode.com/todos'
     todo_info = requests.get(todo_info_url, params=todo_params)
     todos = todo_info.json()
-
-    for i in todos:
-        print(i)
+    filename = '{}.csv'.format(user_id)
+    with open(filename, 'w') as file:
+        writer = csv.writer(file)
+        for row in todos:
+            writer.writerow([user_id, employee_name, 
+            row.get('completed'), row.get('title')])
