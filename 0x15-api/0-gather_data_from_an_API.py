2392bae9154894384ad8f66c3039e4cad17eafe8
King-AJr <talk2king.aj@gmail.com>
2023-04-24 14:49:53 +0100

    use request library instead

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,26 @@
-#!/usr/bin/python3
-""" takes employee's id and returns
-    information about his/her TODO list progress.
-"""
-
-import urllib.request as r
-import urllib.parse as parse
+import requests
 import sys
-import json
 
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    count = 0
+    params = {'id': user_id}
+    todo_params = {'userId': user_id}
+    user_info_url = 'https://jsonplaceholder.typicode.com/users'
+    user_info = requests.get(user_info_url, params=params)
+    employee = user_info.json()[0]
+    employee_name = employee['name']
 
-user_id = sys.argv[1]
-count = 0
-params = parse.urlencode({'id' : user_id})
-todo_param = parse.urlencode({'userId': user_id})
-user_info_url = 'https://jsonplaceholder.typicode.com/users'
-user_info = f"{user_info_url}?{params}"
-with r.urlopen(user_info) as response:
-    employee = json.loads(response.read())
-employee_name = employee[0]['name']
+    todo_info_url = 'https://jsonplaceholder.typicode.com/todos'
+    todo_info = requests.get(todo_info_url, params=todo_params)
+    todos = todo_info.json()
 
+    for i in todos:
+        if i['completed'] == True:
+            count += 1
 
+    print(f"Employee {employee_name} is done with tasks({count}/{len(todos)}):")
+    for i in todos:
+        if i['completed'] == True:
+            print(f"\t{i['title']}")
 
-todo_info_url = 'https://jsonplaceholder.typicode.com/todos'
-todo_info = f"{todo_info_url}?{todo_param}"
-with r.urlopen(todo_info) as todo:
-    todos = json.loads(todo.read())
-
-for i in todos:
-    if i['completed'] == True:
-        count += 1
-
-
-print(f"Employee {employee_name} is done with tasks({count}/{len(todos)}):")
-for i in todos:
-    if i['completed'] == True:
-        print(f"\t{i['title']}")
