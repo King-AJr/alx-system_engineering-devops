0b7a018bb10d4990ff88db27937603b1ea7e2928
King-AJr <talk2king.aj@gmail.com>
2023-05-12 20:39:39 +0100

    reformat code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,15 +5,16 @@ import json
 
 
 def top_ten(subreddit):
-    """ queries the Reddit API and 
-        prints the titles of the first 
-        10 hot posts listed for a given subreddit."""
+    """queries the Reddit API and
+    prints the titles of the first
+    10 hot posts listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "my_reddit_api_v1"}
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers,params=params, allow_redirects=False)
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get('data')
-        [print(c.get('data').get('title')) for c in data.get('children')]
+        data = response.json().get("data")
+        [print(c.get("data").get("title")) for c in data.get("children")]
     else:
-        print ('None')
+        print("None")
